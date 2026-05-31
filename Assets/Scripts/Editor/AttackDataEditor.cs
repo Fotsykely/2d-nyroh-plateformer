9944ce7767e76aa275a45c93eb1d8de4c4f8ae24
AndoRAZAFY <ando@natixgroup.com>
2026-05-31 21:29:57 +0300

    feat(editor): add AttackDataEditor for custom inspector

---
+++ Assets/Scripts/Editor/AttackDataEditor.cs
@@ -0,0 +1,38 @@
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(AttackData))]
+[CanEditMultipleObjects]
+public class AttackDataEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update();
+
+        EditorGUILayout.PropertyField(serializedObject.FindProperty("attackDuration"));
+        EditorGUILayout.PropertyField(serializedObject.FindProperty("damage"));
+
+        EditorGUILayout.Space();
+
+        var shapeProp = serializedObject.FindProperty("shape");
+        EditorGUILayout.PropertyField(shapeProp);
+        EditorGUILayout.PropertyField(serializedObject.FindProperty("hitboxOffset"));
+
+        bool isBox = shapeProp.enumNames[shapeProp.enumValueIndex] == "Box";
+
+        if (isBox)
+        {
+            EditorGUILayout.PropertyField(serializedObject.FindProperty("hitboxSize"));
+            EditorGUILayout.PropertyField(serializedObject.FindProperty("hitboxRotation"));
+        }
+        else
+        {
+            EditorGUILayout.PropertyField(serializedObject.FindProperty("hitboxRadius"));
+        }
+
+        EditorGUILayout.Space();
+        EditorGUILayout.PropertyField(serializedObject.FindProperty("gizmoColor"));
+
+        serializedObject.ApplyModifiedProperties();
+    }
+}
